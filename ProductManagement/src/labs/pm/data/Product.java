778c9b393b8f92f36401d27f28e47e0120cbbf62
Abdel Galeb <abdel.galeb@fatec.sp.gov.br>
2021-04-16 11:04:10 -0300

    Done module ClassDesign

--- ProductManagement/src/labs/pm/data/Product.java
+++ ProductManagement/src/labs/pm/data/Product.java
@@ -17,51 +17,71 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import static java.math.RoundingMode.HALF_UP;
-
-
+import static labs.pm.data.Rating.*;
 
 /**
  *
  * @author hajadalaj
  */
 public class Product {
-    
+
     public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
     private int id;
-    private String name;       
+    private String name;
     private BigDecimal price;
+    private Rating rating;
+
+    public Product() {
+        this(0, "no name", BigDecimal.ZERO);
+    }
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+
+    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(final int id) {
-        this.id = id;
-    }
-
+//    public void setId(final int id) {
+//        this.id = id;
+//    }
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
+//    public void setName(final String name) {
+//        this.name = name;
+//    }
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
-    }
-    
-    public BigDecimal getDiscount(){
-        
+//    public void setPrice(final BigDecimal price) {
+//        this.price = price;
+//    }
+    public BigDecimal getDiscount() {
+
         return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
-        
+
     }
-    
-    
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public Product applyRating(Rating newRating) {
+        return new Product(this.id, this.name, this.price, newRating);
+
+    }
+
 }
