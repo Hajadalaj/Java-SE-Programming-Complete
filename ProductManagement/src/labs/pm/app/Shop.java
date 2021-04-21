fefb779b678df1c6fdd51374a2012903e8ef370c
Abdel Galeb <abdel.galeb@fatec.sp.gov.br>
2021-04-21 19:59:27 -0300

    Done module Handle Exceptions and Fix Bugs

--- ProductManagement/src/labs/pm/app/Shop.java
+++ ProductManagement/src/labs/pm/app/Shop.java
@@ -39,35 +39,41 @@ public class Shop {
         ProductManager pm = new ProductManager("pt-BR");
 
 //        Product p1 = pm.createProduct(101, "Chopp", BigDecimal.valueOf(3.99), Rating.NOT_RATED, true);
-//        pm.printProductReport(101);
-        pm.createProduct(101, "Chopp", BigDecimal.valueOf(3.99), Rating.NOT_RATED, true);
-        pm.reviewProduct(101, Rating.FIVE_STAR, "The best chopp of the world");
-        pm.reviewProduct(101, Rating.FIVE_STAR, "So cold");
-        pm.reviewProduct(101, Rating.FIVE_STAR, "The better of the country");
-        pm.reviewProduct(101, Rating.THREE_STAR, "Very strong");
-        pm.reviewProduct(101, Rating.ONE_STAR, "I don't like that");
-//        pm.printProductReport(101);
+        pm.printProductReport(101);
+//        pm.createProduct(101, "Chopp", BigDecimal.valueOf(3.99), Rating.NOT_RATED, true);
+        pm.parseProduct("D,101,Chopp,3.99,0,true");
+        pm.printProductReport(101);
+        pm.parseReview("101,5,The best chopp of the world");
+        pm.printProductReport(101);
+        pm.parseProduct("F,102,Cake,2.99,0,2019-09-19");
+        pm.printProductReport(102);
+        pm.parseReview("102,1,No taste of 'grandma' cake");
+//        pm.reviewProduct(101, Rating.FIVE_STAR, "The best chopp of the world");
+//        pm.reviewProduct(101, Rating.FIVE_STAR, "So cold");
+//        pm.reviewProduct(101, Rating.FIVE_STAR, "The better of the country");
+//        pm.reviewProduct(101, Rating.THREE_STAR, "Very strong");
+//        pm.reviewProduct(101, Rating.ONE_STAR, "I don't like that");
+        pm.printProductReport(102);
 
-        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(2.99), Rating.NOT_RATED, LocalDate.now().plusDays(3));
+//        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(2.99), Rating.NOT_RATED, LocalDate.now().plusDays(3));
 //        pm.printProductReport(p3);
-        p3 = pm.reviewProduct(p3, Rating.ONE_STAR, "No taste of 'grandma' cake");
-        p3 = pm.reviewProduct(p3, Rating.THREE_STAR, "Is good");
-        p3 = pm.reviewProduct(p3, Rating.FIVE_STAR, "Delicious");
+//        p3 = pm.reviewProduct(p3, Rating.ONE_STAR, "No taste of 'grandma' cake");
+//        p3 = pm.reviewProduct(p3, Rating.THREE_STAR, "Is good");
+//        p3 = pm.reviewProduct(p3, Rating.FIVE_STAR, "Delicious");
 //        pm.printProductReport(p3);
-
-        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR, false);
-        p2 = pm.reviewProduct(p2, Rating.THREE_STAR, "Hot and taste =P");
-        p2 = pm.reviewProduct(p2, Rating.THREE_STAR, "Very well");
-        p2 = pm.reviewProduct(p2, Rating.FOUR_STAR, ":)");
+//
+//        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR, false);
+//        p2 = pm.reviewProduct(p2, Rating.THREE_STAR, "Hot and taste =P");
+//        p2 = pm.reviewProduct(p2, Rating.THREE_STAR, "Very well");
+//        p2 = pm.reviewProduct(p2, Rating.FOUR_STAR, ":)");
 //        pm.printProductReport(p2);
-
-        Comparator<Product> sorteByRating = (i1, i2) -> i2.getRating().ordinal() - i1.getRating().ordinal();
-        Comparator<Product> sorteByPrice = (i1, i2) -> i2.getPrice().compareTo(i1.getPrice());
-
-        pm.printProducts(p -> p.getDiscount().floatValue() == 0, sorteByRating.thenComparing(sorteByPrice));
-        pm.getDiscounts().forEach((rating, discount) -> System.out.println(rating + "\t" + discount));
+//
+//        Comparator<Product> sorteByRating = (i1, i2) -> i2.getRating().ordinal() - i1.getRating().ordinal();
+//        Comparator<Product> sorteByPrice = (i1, i2) -> i2.getPrice().compareTo(i1.getPrice());
+//
+//        pm.printProducts(p -> p.getDiscount().floatValue() == 0, sorteByRating.thenComparing(sorteByPrice));
+//        pm.getDiscounts().forEach((rating, discount) -> System.out.println(rating + "\t" + discount));
 //        pm.printProducts(sorteByPrice.thenComparing(sorteByRating).reversed());
-
 //        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
 //        Product p4 = pm.createProduct(105, "Vodka", BigDecimal.valueOf(10.00), Rating.FIVE_STAR, true);
 //        Product p5 = p3.applyRating(Rating.THREE_STAR);
--- ProductManagement/src/labs/pm/data/ProductManager.java
+++ ProductManagement/src/labs/pm/data/ProductManager.java
@@ -19,8 +19,10 @@ package labs.pm.data;
 import java.math.BigDecimal;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -32,6 +34,8 @@ import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 /**
@@ -42,12 +46,17 @@ public class ProductManager {
 
     private Map<Product, List<Review>> products = new HashMap();
     private ResourceFormatter formatter;
+    private ResourceBundle config = ResourceBundle.getBundle("labs.pm.data.config");
+    private MessageFormat reviewFormat = new MessageFormat(config.getString("review.data.format"));
+    private MessageFormat productFormat = new MessageFormat(config.getString("product.data.format"));
     private static Map<String, ResourceFormatter> formatters
             = Map.of("en-GB", new ResourceFormatter(Locale.UK),
                     "en-US", new ResourceFormatter(Locale.US),
                     "fr-FR", new ResourceFormatter(Locale.FRANCE),
                     "es-ES", new ResourceFormatter(new Locale("es", "ES")),
                     "pt-BR", new ResourceFormatter(new Locale("pt", "BR")));
+
+    private static final Logger logger = Logger.getLogger(ProductManager.class.getName());
 
     public ProductManager(Locale locale) {
         this(locale.toLanguageTag());
@@ -78,7 +87,12 @@ public class ProductManager {
     }
 
     public Product reviewProduct(int id, Rating rating, String comments) {
-        return reviewProduct(findProduct(id), rating, comments);
+        try {
+            return reviewProduct(findProduct(id), rating, comments);
+        } catch (ProductManagerException ex) {
+            logger.log(Level.INFO, ex.getMessage());
+        }
+        return null;
     }
 
     public Product reviewProduct(Product product, Rating rating, String comments) {
@@ -97,7 +111,11 @@ public class ProductManager {
     }
 
     public void printProductReport(int id) {
-        printProductReport(findProduct(id));
+        try {
+            printProductReport(findProduct(id));
+        } catch (ProductManagerException ex) {
+            logger.log(Level.INFO, ex.getMessage());
+        }
     }
 
     public void printProductReport(Product product) {
@@ -135,7 +153,7 @@ public class ProductManager {
         System.out.println(txt);
     }
 
-    public Product findProduct(int id) {
+    public Product findProduct(int id) throws ProductManagerException {
 //        for (Product product : products.keySet()) {
 //            if (product.getId() == id) {
 //                return product;
@@ -144,7 +162,9 @@ public class ProductManager {
 //        return null;
         return products.keySet().stream()
                 .filter(p -> p.getId() == id)
-                .findFirst().orElseGet(() -> null);
+                .findFirst()
+                .orElseThrow(() -> new ProductManagerException("Poduct id: " + id + " notfound"));
+        //orElseGet(() -> null);
     }
 
     public Map<String, String> getDiscounts() {
@@ -157,6 +177,37 @@ public class ProductManager {
                                         Collectors.summingDouble(
                                                 p -> p.getDiscount().doubleValue()),
                                         d -> formatter.moneyFormat.format(d))));
+    }
+
+    public void parseReview(String text) {
+        try {
+            Object[] values = reviewFormat.parse(text);
+            reviewProduct(Integer.parseInt((String) values[0]), Rateable.convert(Integer.parseInt((String) values[1])), (String) values[2]);
+        } catch (ParseException | NumberFormatException ex) {
+            logger.log(Level.WARNING, "Error parse review " + text, ex);
+        }
+    }
+
+    public void parseProduct(String text) {
+        try {
+            Object[] values = productFormat.parse(text);
+            int id = Integer.parseInt((String) values[1]);
+            String name = (String) values[2];
+            BigDecimal price = BigDecimal.valueOf(Double.parseDouble((String) values[3]));
+            Rating rating = Rateable.convert(Integer.parseInt((String) values[4]));
+            switch ((String) values[0]) {
+                case "D":
+                    boolean alcoholic = Boolean.parseBoolean((String) values[5]);
+                    createProduct(id, name, price, rating, alcoholic);
+                    break;
+                case "F":
+                    LocalDate bestBefore = LocalDate.parse((String) values[5]);
+                    createProduct(id, name, price, rating, bestBefore);
+            }
+        } catch (ParseException | NumberFormatException | DateTimeParseException ex) {
+            logger.log(Level.WARNING, "Error parsing product " + text + " " + ex.getMessage());
+        }
+
     }
 
     private static class ResourceFormatter {
